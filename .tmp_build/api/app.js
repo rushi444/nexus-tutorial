cb3b2ff5a84cde38bb5e15f8aa29114674e8eb7f
rushi444 <rushi44@gmail.com>
2020-08-12 09:39:41 -0700

    trying to deploy

---
+++ .tmp_build/api/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nexus_1 = require("nexus");
+const nexus_plugin_prisma_1 = require("nexus-plugin-prisma");
+nexus_1.use(nexus_plugin_prisma_1.prisma());
